bfb1d3e30a6d88c13dbd56a975398c429f5836b4
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2021-10-09 06:05:37 -0300

    netlify test

--- js/api.js
+++ js/api.js
@@ -1,7 +1,10 @@
+const API_URL = "/.netlify/functions/API_URL";
+const API_TOKEN = "/.netlify/functions/API_TOKEN";
+
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url":  API_URL,
+	"url": API_URL,
 	"method": "GET",
 	"headers": {
 		"accept-language": "us",
