50d694339d0b8a1f1dbf6b6010074dca66a2eaf1
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2021-10-09 05:18:02 -0300

    API_URL added

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
 import API_TOKEN from "./key.js"
 
+const API_URL = "https://bing-news-search1.p.rapidapi.com/news?originalImg=true&cc=us&safeSearch=Off&textFormat=Raw";
+
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://bing-news-search1.p.rapidapi.com/news?originalImg=true&cc=us&safeSearch=Off&textFormat=Raw",
+	"url":  API_URL,
 	"method": "GET",
 	"headers": {
 		"accept-language": "us",
