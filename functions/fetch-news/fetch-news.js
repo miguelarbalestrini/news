2dd401822e0ff03e5f09315dcd124443d4c163ac
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2021-10-11 03:14:41 -0300

    fetch improved & language changed to english

--- functions/fetch-news/fetch-news.js
+++ functions/fetch-news/fetch-news.js
@@ -1,23 +1,22 @@
 const process = require('process')
 const axios = require('axios')
-const qs = require('qs')
+
+const { API_TOKEN, API_URL } = process.env
+
+const settings =  {
+  method: 'GET',
+  url: API_URL,
+  params: { originalImg: 'true', cc: 'us', safeSearch: 'Off' },
+  "headers": {
+    "accept-language": "us",
+    "x-bingapis-sdk": "true",
+    "x-search-location": "us",
+    "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+    "x-rapidapi-key": API_TOKEN
+  }
+}
 
 const handler = async function (event) {
-
-  const { API_TOKEN, API_URL } = process.env
-
-  const settings =  {
-    method: 'GET',
-    url: API_URL,
-    params: { safeSearch: 'Off' },
-    "headers": {
-      "accept-language": "us",
-      "x-bingapis-sdk": "true",
-      "x-search-location": "us",
-      "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-      "x-rapidapi-key": API_TOKEN
-    }
-  }
 
   try {
     const { data } = await axios(settings)
